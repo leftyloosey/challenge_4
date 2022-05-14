7e9da73b81fbf595720094f04ba5d5bce02da6c6
davidhardin <thisgoestodavid@gmail.com>
2022-05-14 16:18:43 -0600

    last commit

--- readme.md
+++ readme.md
@@ -2,3 +2,18 @@
 
 ## Hello
 
+This was a tough one. I actually haven't quite finished the scoreboard because 
+I am still figuring out localStorage, but, alas, I am out of time for now. I would have liked to clean up the code a bit, too, but I don't have the night off. 
+
+It works for the most part, though. A fairly simple function that runs through an array of questions while a timer counts down. It was not fairly simple to write. I spent forever trying to make the indices of loops match up, etc. I like my little
+hack of using the hidden string of spaces to mark the correct answer.
+
+If I had more time I would make it pretty in addition to making it work. 
+
+## Link
+
+[index]()
+
+## Screen
+
+!(screen.png)
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//get elements and query selectors and variables 
+//getElements and query selectors and variables 
 
 var qButton1 = document.getElementById("question1")
 var qButton2 = document.getElementById("question2")
 var qButton3 = document.getElementById("question3")
 var qButton4 = document.getElementById("question4")
 
+var qWrap = document.getElementById("qWrap")
+var highDisplay = document.getElementById("hiscores")
 var timerDisplay = document.getElementById("timer")
 var qListEl = document.getElementById("questionList")
 var startButton = document.getElementById("startButton")
@@ -13,11 +15,27 @@ var questionList = document.getElementById("questionList")
 var middle = document.getElementById("middle")
 var secondMiddle = document.getElementById("secondMiddle")
 var questionBody = document.getElementById("questionBody")
+var scoreText = document.querySelector("scoreText")
 
+var scoreArray = []
+var highWin = ""
+var winArray = []
+var winName = ""
+var gotName = ""
+
+var pTag = document.createElement("p")
 var liTag = document.createElement("p")
 
+var scoreButton = document.createElement("button")
+var againButton = document.createElement("button")
+var scoreName = document.createElement("input")
+
+var ofArrays = []
+
+var bigInterval = 0
+var score = 0
 var bigIndex = 0
-timer = 60
+var timer = 60
 var rightAnswer = ""
 
 var questionArray = []
@@ -45,8 +63,8 @@ var oopQuestion = [
     "What does OOP stand for?",
     "Only Our Potatoes",
     "Onward, Or Perish!",
+    "Object-oriented Programming       ",
     "Owls, Omens, Premonitions",
-    "Object-oriented Programming       "
 ]
 
 var loopQuestion = [
@@ -62,41 +80,31 @@ var cssQuestion = [
     "Certified Stamp Seller",
     "Cranial Stop Serum",
     "Cash So Soggy",
-    "Cascading Style Sheet"
+    "Cascading Style Sheet       "
 ]
 
-// and all the functions here.
 
-function removeChild() {
-    questionText.removeChild(liTag)
-}
-
-function isRight(right) {
-    return right.includes("       ")
-}
-
+// startTimer has the countdown function and failing timeout condition
 function startTimer() {
     timerDisplay.style.visibility = "visible"
     timerDisplay.textContent = timer 
     
     function countDown() {
-        timer = timer-1
-        
+        timer--
         timerDisplay.textContent = timer
-        if (timer <= 0 ) {
-            console.log("fail")
-            questionBody.appendChild(liTag)
-            liTag.textContent = "find a different career"
-            setTimeout(removeChild, 2000)
-            return
-        }
+        if (timer <= 0 ){failScreen()}
+        
     }
-    setInterval(countDown, 1000)
 
-   
+    bigInterval = setInterval(countDown, 1000)
 }   
+function clearRestart() {
+    location.reload()
+    startUp()
+}
 
-
+// appendQuestions is one of the two main driving functions, and assigns question 
+// data to the answer buttons.
 function appendQuestions(a) {
     
     function qToQ() {
@@ -105,27 +113,23 @@ function appendQuestions(a) {
         qButtonArray[2].textContent = a[3]
         qButtonArray[3].textContent = a[4]
     }
-
     questionBody.textContent = a[0]
     questionList.style.display = "flex";
+    questionList.style.visibility = "visible"
+    questionBody.style.visibility = "visible"
 
     a.forEach(qToQ)
-    console.log(qButtonArray, a)
-    
+ 
     rightAnswer = a.find(isRight)
-    console.log(rightAnswer)
-    console.log(a)
-    console.log(a.indexOf(rightAnswer))
     goodIndex = a.indexOf(rightAnswer)
-    console.log("good index" , goodIndex)
     
-    console.log(bigIndex)
 
+    
     qButtonArray[0].removeEventListener("click", throughQuestions)
     qButtonArray[1].removeEventListener("click", throughQuestions)
     qButtonArray[2].removeEventListener("click", throughQuestions)
     qButtonArray[3].removeEventListener("click", throughQuestions)
-
+    
 
     qButtonArray[0].addEventListener("click", wrongQuestion)
     qButtonArray[1].addEventListener("click", wrongQuestion)
@@ -140,6 +144,18 @@ function appendQuestions(a) {
 }
 var allQuestions = [seoQuestion, javaQuestion, oopQuestion, loopQuestion, cssQuestion]
 
+
+// throughQuestions is the other main function. it records a win and ends the 
+// game when enough wins are accrued.
+function throughQuestions() {
+    score++
+    bigIndex++
+    if (score >= 4) {winScreen()}
+    appendQuestions(allQuestions[bigIndex])
+}
+
+// this one subtracts time each time a question is wrong, and registers 
+// onscreen that the answer was incorrect 
 function wrongQuestion() {
     timer = timer - 10 
     
@@ -147,26 +163,97 @@ function wrongQuestion() {
     liTag.setAttribute("style", "font-weight: bold;")
     questionText.appendChild(liTag)
   
-    setTimeout(removeChild, 850)
+    setTimeout(removeChild, 600)
 }
 
-function throughQuestions() {
-    bigIndex++
-    appendQuestions(allQuestions[bigIndex])
+// next few are for dealing with the endgame and highscores
+function failScreen() {
+    questionList.style.visibility = "hidden"
+    questionBody.style.display= "none"
+    
+    clearInterval(bigInterval)
+    secondMiddle.appendChild(pTag)
+    
+    alert("You answered: " + score + " questions")
+    liTag.textContent = "find a different career."
+    questionText.appendChild(liTag)
 }
 
-function questionOne() {
+function winScreen() {
+    clearInterval(bigInterval)
+    qWrap.style.display = "none"
+    qWrap.style.visibility = "hidden"
+    questionBody.style.display = "none"
+    
+    liTag.setAttribute("style", "font-weight: bold;")
+    liTag.textContent = "you have conquered."
+    secondMiddle.appendChild(liTag)
+    scoreName.setAttribute("type", "text");
+    secondMiddle.appendChild(scoreName)
+    scoreName.style.visibility = "visible"
+
+    scoreButton.textContent = "record a name for the board"
+    secondMiddle.appendChild(scoreButton)
+   
+    againButton.style.visibility = "visible"
+    againButton.textContent = "play again"
+    secondMiddle.appendChild(againButton)
+
+    console.log(score, timer)
+
+
+    scoreButton.addEventListener("click", recordHigh)
+    againButton.addEventListener("click", clearRestart)
+}
+    
+function recordHigh() { 
+    console.log(scoreName.value, score, timer)
+    
+    
+    winArray.push(scoreName.value, score, timer)
+
+    ofArrays.push(winArray)
+
+    localStorage.setItem("initLocal", ofArrays)
+    
+    var wiki1 = localStorage.getItem("initLocal")
+    
+    ofArrays.push(wiki1)
+
+    localStorage.setItem("secLocal", ofArrays)
+
+    var wiki2 = localStorage.getItem('secLocal')
+
+    scoreArray.push(wiki2)
+
+    console.log(scoreArray)
+
+    
+
+    scoreName.value = ""
+    scoreName.style.visibility = "hidden"
+    secondMiddle.removeChild(scoreButton)
+    
+}
+
+function removeChild() {
+    questionText.removeChild(liTag)
+}
+
+function isRight(right) {
+    return right.includes("       ") // the correct answer button is indicated by
+                                     // this string of spaces which is hidden
+                                     // onscreen
+}
+
+// initializes the game, starts the timer
+function startUp() {
     middle.style.visibility = "hidden"
     startButton.style.display = "none"
+
     startTimer()
     appendQuestions(seoQuestion)
 }
 
 
-
-
-
-  
-
-startButton.addEventListener("click", questionOne)
-
+startButton.addEventListener("click", startUp)
